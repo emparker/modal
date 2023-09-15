5d4e988763ea08cc11f3167258bba05e6d33437d
Emily Parker <emilyarparker@gmail.com>
2023-09-15 16:13:08 -0600

    first commit.  orientation and set up.

---
+++ app/components/DownArrow.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DownArrow() {
+    return (
+        <div className="flex items-center justify-center h-16 animate-bounce w-30">
+            <div className="w-30 h-0 border-t-8 border-r-8 border-transparent border-gray-900"><span className="">click card</span></div>
+        </div>
+    )
+}
